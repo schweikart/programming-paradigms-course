571f471c1ecedbe9ce13905b05ee1feeda46888b
Max Schweikart <hello@maxschweik.art>
2023-03-11 19:23:51 +0100

    feat(sheet01): implement pow2 and basic tests

--- sheet01/Arithmetik.hs
+++ sheet01/Arithmetik.hs
@@ -1,6 +1,16 @@
 module Arithmetik where
     -- Task 1.1: simple pow implementation for integers
+    -- time complexity: O(e)
     pow1 b e
         | e == 0    = 1
         | e > 0     = b * pow1 b (e-1)
         | otherwise = error "negative exponent"
+
+    -- Task 1.2: more efficient pow implementation taking advantage of the
+    -- following equation: b^(2e) = (b^2)^e.
+    -- time complexity: O(log2 e)
+    pow2 b e
+        | e < 0     = error "negative exponent"
+        | e == 0    = 1
+        | even e    = pow2 (b * b) (e `div` 2)
+        | otherwise = b * pow2 b (e-1)
--- sheet01/tests/TestArithmetik.hs
+++ sheet01/tests/TestArithmetik.hs
@@ -1,9 +1,20 @@
 module TestArithmetik where
     import TestUtils (testEq)
-    import Arithmetik (pow1)
+    import Arithmetik (pow1, pow2)
     
     pow1Tests = [
             testEq "pow1 2 10 == 1024" 1024 (pow1 2 10),
             testEq "pow1 10 10 == 10000000000" 10000000000 (pow1 10 10),
             testEq "pow1 99999 0 == 1" 1 (pow1 99999 0)
         ]
+    
+    pow2Tests = [
+            testEq "pow2 2 10 == 1024" 1024 (pow2 2 10),
+            testEq "pow2 10 10 == 10000000000" 10000000000 (pow2 10 10),
+            testEq "pow2 99999 0 == 1" 1 (pow2 99999 0),
+            testEq "pow2 14 15 == pow1 14 15" (pow1 14 15) (pow2 14 15)
+        ]
+
+    arithmeticTestCases = 
+        pow1Tests ++
+        pow2Tests
