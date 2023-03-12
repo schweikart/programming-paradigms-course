c12d061fe177f8f90560656fd123841e2b99f528
Max Schweikart <hello@maxschweik.art>
2023-03-12 12:29:46 +0100

    docs(sheet01): specify time complexity of pow1 and pow2 more precisely

--- sheet01/Arithmetik.hs
+++ sheet01/Arithmetik.hs
@@ -1,6 +1,6 @@
 module Arithmetik where
     -- Task 1.1: simple pow implementation for integers
-    -- time complexity: O(e)
+    -- time complexity: Theta(e)
     pow1 b e
         | e == 0    = 1
         | e > 0     = b * pow1 b (e-1)
@@ -8,7 +8,7 @@ module Arithmetik where
 
     -- Task 1.2: more efficient pow implementation taking advantage of the
     -- following equation: b^(2e) = (b^2)^e.
-    -- time complexity: O(log2 e)
+    -- time complexity: Theta(log e)
     pow2 b e
         | e < 0     = error "negative exponent"
         | e == 0    = 1
