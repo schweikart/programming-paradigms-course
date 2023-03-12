382974d52cf572b65e3910dd94abb9571ed1bc23
Max Schweikart <hello@maxschweik.art>
2023-03-12 11:30:43 +0100

    feat(sheet01): implement merge

--- sheet01/Sort.hs
+++ sheet01/Sort.hs
@@ -7,3 +7,9 @@ module Sort where
 
     -- Task 2.2: sort a given list using the insertion sort algorithm
     insertSort ps = foldr insert [] ps
+
+    -- Task 2.3: merge two sorted integer lists
+    merge (x:xs) (y:ys)
+        | x < y         = x:merge xs (y:ys)
+        | x >= y        = y:merge (x:xs) ys
+    merge xs ys         = xs ++ ys -- one of them is empty
--- sheet01/tests/TestSort.hs
+++ sheet01/tests/TestSort.hs
@@ -1,5 +1,5 @@
 module TestSort where
-    import Sort (insert, insertSort)
+    import Sort (insert, insertSort, merge)
     import TestUtils (testEq)
 
     typedEmptyList :: [Integer]
@@ -19,4 +19,16 @@ module TestSort where
             testEq "insertSort [8,4,2,6,1,4,3,7,3] == [1,2,3,3,4,4,6,7,8]" [1,2,3,3,4,4,6,7,8] (insertSort [8,4,2,6,1,4,3,7,3])
         ]
     
-    sortTestCases = insertTests ++ insertSortTests
+    mergeTests = [
+            testEq "merge [] [] == []" [] (merge [] typedEmptyList),
+            testEq "merge [10] [] == [10]" [10] (merge [10] []),
+            testEq "merge [] [10] == [10]" [10] (merge [] [10]),
+            testEq "merge [10] [10] == [10, 10]" [10, 10] (merge [10] [10]),
+            testEq "merge [9] [10] == [9, 10]" [9, 10] (merge [9] [10]),
+            testEq "merge [11] [10] == [10, 11]" [10, 11] (merge [11] [10]),
+            testEq "merge [0,2,4,6,8] [1,3,5,7,9] == [0..9]" [0..9] (merge [0,2,4,6,8] [1,3,5,7,9]),
+            testEq "merge [0,1,3,4,5] [2,3,5,6,9] == [0,1,2,3,3,4,5,5,6,9]" [0,1,2,3,3,4,5,5,6,9] (merge [0,1,3,4,5] [2,3,5,6,9]),
+            testEq "merge [4,5] [0..9] == [0,1,2,3,4,4,5,5,6,7,8,9]" [0,1,2,3,4,4,5,5,6,7,8,9] (merge [4,5] [0..9])
+        ]
+    
+    sortTestCases = insertTests ++ insertSortTests ++ mergeTests
