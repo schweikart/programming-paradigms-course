5e7a77a644673106995f1016b7f9b6e21deca760
Max Schweikart <hello@maxschweik.art>
2023-03-12 09:34:24 +0100

    test(sheet01): add error tests for pow3

--- sheet01/tests/TestArithmetik.hs
+++ sheet01/tests/TestArithmetik.hs
@@ -1,6 +1,7 @@
 module TestArithmetik where
-    import TestUtils (testEq)
-    import Arithmetik (pow1, pow2)
+    import TestUtils (testEq, testThrows)
+    import Arithmetik (pow1, pow2, pow3)
+    import Control.Exception (try, evaluate)
     
     pow1Tests = [
             testEq "pow1 2 10 == 1024" 1024 (pow1 2 10),
@@ -12,14 +13,16 @@ module TestArithmetik where
             testEq "pow2 2 10 == 1024" 1024 (pow2 2 10),
             testEq "pow2 10 10 == 10000000000" 10000000000 (pow2 10 10),
             testEq "pow2 99999 0 == 1" 1 (pow2 99999 0),
-            testEq "pow2 14 15 == pow1 14 15" (pow1 14 15) (pow2 14 15)
+            testEq "pow2 14 15 == pow1 14 15" (pow1 14 15) (pow2 14 15),
+            testThrows "pow2 10 -1 throws an error" (try (evaluate (pow2 10 (-1 :: Integer))))
         ]
     
     pow3Tests = [
-            testEq "pow3 2 10 == 1024" 1024 (pow2 2 10),
-            testEq "pow3 10 10 == 10000000000" 10000000000 (pow2 10 10),
-            testEq "pow3 99999 0 == 1" 1 (pow2 99999 0),
-            testEq "pow3 14 15 == pow1 14 15" (pow1 14 15) (pow2 14 15)
+            testEq "pow3 2 10 == 1024" 1024 (pow3 2 10),
+            testEq "pow3 10 10 == 10000000000" 10000000000 (pow3 10 10),
+            testEq "pow3 99999 0 == 1" 1 (pow3 99999 0),
+            testEq "pow3 14 15 == pow1 14 15" (pow1 14 15) (pow3 14 15),
+            testThrows "pow3 10 -1 throws an error" (try (evaluate (pow3 10 (-1 :: Integer))))
         ]
 
     arithmeticTestCases = 
