ffe59a0e97ae09e35d0f5264b82768651ad3b3ac
Max Schweikart <hello@maxschweik.art>
2023-03-13 18:59:12 +0100

    feat(sheet03): implement primepowers (+ tests)

--- sheet03/Merge.hs
+++ sheet03/Merge.hs
@@ -7,3 +7,14 @@ module Merge where
         | otherwise = y : merge (x:xs) ys
     merge [] ys     = ys
     merge xs []     = xs
+
+    -- Task 3.2: returns a sorted stream of powers p^i of all primes p for
+    -- exponents 1 <= i <= p.
+    primepowers :: Integer -> [Integer]
+    primepowers n = foldr merge [] [map (^ i) primes | i <- [1..n]] where
+        -- prime sieve implementation from the lecture, slightly modified
+        primes :: [Integer]
+        primes = sieve [2..]
+        sieve [] = []
+        sieve (p:xs)    = p : sieve (filter (not . multipleOf p) xs)
+        multipleOf p x = x `mod` p == 0
--- sheet03/tests/TestMerge.hs
+++ sheet03/tests/TestMerge.hs
@@ -1,12 +1,21 @@
 module TestMerge (mergeTestCases) where
     import TestUtils (testEq)
-    import Merge (merge)
+    import Merge (merge, primepowers)
     
     mergeTests = [
             testEq "take 10 (merge [0..] [0..]) == [0,0,1,1,2,2,3,3,4,4]" [0,0,1,1,2,2,3,3,4,4] $ take 10 $ merge [0..] [0..],
             testEq "take 10 (merge [0..] [2]) == [0,1,2,2,3,4,5,6,7,8]" [0,1,2,2,3,4,5,6,7,8] $ take 10 $ merge [0..] [2],
             testEq "merge [1,2] [3,4] == [1..4]" [1..4] $ merge [1,2] [3,4]
-
+        ]
+    
+    primepowersTests = [
+            testEq "primepowers 0 == []" [] $ primepowers 0,
+            testEq "take 10 (primepowers 1) == [2,3,5,7,11,13,17,19,23,29]" [2,3,5,7,11,13,17,19,23,29] $ take 10 $ primepowers 1,
+            testEq "take 10 (primepowers 2) == [2,3,4,5,7,9,11,13,17,19]" [2,3,4,5,7,9,11,13,17,19] $ take 10 $ primepowers 2,
+            testEq "take 10 (primepowers 3) == [2,3,4,5,7,8,9,11,13,17]" [2,3,4,5,7,8,9,11,13,17] $ take 10 $ primepowers 3,
+            testEq "take 10 (primepowers 4) == [2,3,4,5,7,8,9,11,13,16]" [2,3,4,5,7,8,9,11,13,16] $ take 10 $ primepowers 4,
+            testEq "take 10 (primepowers 5) == [2,3,4,5,7,8,9,11,13,16]" [2,3,4,5,7,8,9,11,13,16] $ take 10 $ primepowers 5,
+            testEq "take 10 (primepowers 42) == [2,3,4,5,7,8,9,11,13,16]" [2,3,4,5,7,8,9,11,13,16] $ take 10 $ primepowers 42
         ]
 
-    mergeTestCases = mergeTests
+    mergeTestCases = mergeTests ++ primepowersTests
