02445e983aff3c1dded4cdfd5a473c1ffc802101
Max Schweikart <hello@maxschweik.art>
2023-03-13 16:12:41 +0100

    docs(sheet03): analyze fibs time complexity

--- sheet03/Fibs.hs
+++ sheet03/Fibs.hs
@@ -1,5 +1,8 @@
 module Fibs where
-    -- Task 1: 
+    -- Task 1: create the infinite stream of all fibonacci numbers in ascending
+    -- order.
+    -- Time complexity for accessing the n-th element: Theta(n)
+    -- (assuming additions time complexity is Theta(1))
     fibs :: [Integer]
     fibs = 0:1:fibsFrom 0 1 where
         fibsFrom nMinus2 nMinus1 = n : fibsFrom nMinus1 n where
