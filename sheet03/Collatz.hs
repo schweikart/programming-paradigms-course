aa55d28e561276b8d41dfd7cb0e122b32909cadb
Max Schweikart <hello@maxschweik.art>
2023-03-13 17:57:16 +0100

    docs(sheet03): add docs for collatz maxNum

--- sheet03/Collatz.hs
+++ sheet03/Collatz.hs
@@ -17,6 +17,11 @@ module Collatz (collatz, num, maxNum) where
             | x == 1    = acc
             | otherwise = numAcc (nextCollatzElement x) (acc + 1)
 
+    -- Task 2.3: For maxNum a b (with integers a <= b), this function returns a
+    -- tuple (m, num m) where a <= m <= b is the number so that num m is
+    -- maximum.
+    -- Example: For a=1 and b=1000, the collatz sequence starting with m=871
+    -- has the maximum num. 
     maxNum :: Int -> Int -> (Int,Int)
     maxNum a b
         | a > b             = error "invalid range"
