6a9c01862cf378a8ba67f9c8edfdbdeb5fe28d42
Max Schweikart <hello@maxschweik.art>
2023-03-14 17:15:05 +0100

    feat(tutorial02): implement group (+ tests)

--- tutorial02/RunLengthEncoding.hs
+++ tutorial02/RunLengthEncoding.hs
@@ -1,3 +1,10 @@
 module RunLengthEncoding where
+    -- splits a liste before the first occurrence of the predicate coming true
     splitWhen :: (t -> Bool) -> [t] -> ([t], [t])
     splitWhen p xs = (takeWhile (not . p) xs, dropWhile (not . p) xs)
+
+    -- splits a list into lists that only contain the same element
+    group :: Eq t => [t] -> [[t]]
+    group [] = []
+    group list@(x:xs) = nextGroup : group rest where
+        (nextGroup, rest) = splitWhen (/= x) list
--- tutorial02/tests/TestRunLengthEncoding.hs
+++ tutorial02/tests/TestRunLengthEncoding.hs
@@ -1,8 +1,15 @@
-module TestRunLengthEncoding where
-    import RunLengthEncoding (splitWhen)
+module TestRunLengthEncoding (runLengthEncodingTestCases) where
+    import RunLengthEncoding (splitWhen, group)
     import TestUtils (testEq)
     
-    runLengthEncodingTestCases = [
+    splitWhenTests = [
             testEq "splitWhen even [1,2,3,4,5] == ([1], [2,3,4,5])" ([1], [2,3,4,5]) $ splitWhen even [1,2,3,4,5],
             testEq "splitWhen (== ' ') \"Hello World!\" == (\"Hello\", \" World!\")" ("Hello", " World!") $ splitWhen (== ' ') "Hello World!"
         ]
+    
+    groupTests = [
+            testEq "group [1,1,2,1,3,3,3] == [[1,1], [2], [1], [3,3,3]]" [[1,1], [2], [1], [3,3,3]] $ group [1,1,2,1,3,3,3],
+            testEq "group \"aaabbacccc\" == [\"aaa\", \"bb\", \"a\", \"cccc\"]" ["aaa", "bb", "a", "cccc"] $ group "aaabbacccc"
+        ]
+
+    runLengthEncodingTestCases = splitWhenTests ++ groupTests
