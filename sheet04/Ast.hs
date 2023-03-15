d440a773fb0ce68469d5024b2b605da02e06ffe0
Max Schweikart <hello@maxschweik.art>
2023-03-15 18:49:20 +0100

    feat(tutorial04): implement Less, And and Not in Ast (+ tests)

--- sheet04/Ast.hs
+++ sheet04/Ast.hs
@@ -1,15 +1,25 @@
 module Ast (Exp (..), Env, eval) where
     -- simple integer adding expression
-    data Exp t =            -- t is the type containing all variables
-        Var t |             -- variable from the given type
-        Const Integer |     -- Integer = constant value
-        Sum (Exp t) (Exp t) -- with summand expressions
+    data Exp t =                -- t is the type containing all variables
+        Var t |                 -- variable from the given type
+        Const Integer |         -- Integer = constant value
+        Sum (Exp t) (Exp t) |   -- with summand expressions
+        Less (Exp t) (Exp t) |  -- a < b is 1 iff a is evaluated to less than b, otherwise 1
+        And (Exp t) (Exp t) |   -- a and b is 1 iff a and b are evaluated to something other than 0, otherwise 0
+        Not (Exp t)             -- not a is 1 iff a is 0, otherwise 1
 
     -- variable environment assigning a value to each variable
     type Env t = t -> Integer
 
     -- evaluates a given expression with a given variable assignment
     eval :: Env t -> Exp t -> Integer
-    eval env (Var t) = env t
-    eval env (Const t) = t
-    eval env (Sum t1 t2) = eval env t1 + eval env t2
+    eval env (Var t)        = env t
+    eval env (Const t)      = t
+    eval env (Sum t1 t2)    = eval env t1 + eval env t2
+    eval env (Less t1 t2)   = if eval env t1 < eval env t2 then 1 else 0
+    eval env (And t1 t2)    = if trueish (eval env t1) && trueish (eval env t2) then 1 else 0
+    eval env (Not t)        = if trueish (eval env t) then 0 else 1
+
+    -- checks if an integer is true-ish, i.e., it is something other than zero 
+    trueish :: Integer -> Bool
+    trueish = (/= 0)
--- sheet04/tests/TestAst.hs
+++ sheet04/tests/TestAst.hs
@@ -1,5 +1,5 @@
 module TestAst where
-    import Ast (Env, eval, Exp (Sum, Var, Const))
+    import Ast (Env, eval, Exp (Sum, Var, Const, Less, And, Not))
     import TestUtils (testEq)
     
     a1b2c3 :: Env Char
@@ -8,11 +8,35 @@ module TestAst where
     a1b2c3 'c' = 3
 
     evalTests = [
+            -- var
             testEq "a == 1 with a=1,b=2,c=3" 1 $ eval a1b2c3 (Var 'a'),
+
+            -- const
             testEq "7 == 7 with a=1,b=2,c=3" 7 $ eval a1b2c3 (Const 7),
+
+            -- sum
             testEq "a + 7 == 8 with a=1,b=2,c=3" 8 $ eval a1b2c3 (Sum (Var 'a') (Const 7)),
+
+            -- less
+            testEq "6 < 9 with a=1,b=2,c=3" 1 $ eval a1b2c3 (Less (Const 6) (Const 9)),
+            testEq "9 < 6 with a=1,b=2,c=3" 0 $ eval a1b2c3 (Less (Const 9) (Const 6)),
+            testEq "6 < 6 with a=1,b=2,c=3" 0 $ eval a1b2c3 (Less (Const 6) (Const 6)),
+
+            -- and
+            testEq "0 and 0 with a=1,b=2,c=3" 0 $ eval a1b2c3 (And (Const 0) (Const 0)),
+            testEq "0 and 1 with a=1,b=2,c=3" 0 $ eval a1b2c3 (And (Const 0) (Const 1)),
+            testEq "1 and 0 with a=1,b=2,c=3" 0 $ eval a1b2c3 (And (Const 1) (Const 0)),
+            testEq "1 and 1 with a=1,b=2,c=3" 1 $ eval a1b2c3 (And (Const 1) (Const 1)),
+
+           -- not
+           testEq "not 0 with a=1,b=2,c=3" 1 $ eval a1b2c3 (Not (Const 0)),
+           testEq "not 1 with a=1,b=2,c=3" 0 $ eval a1b2c3 (Not (Const 1)),
+           testEq "not 2 with a=1,b=2,c=3" 0 $ eval a1b2c3 (Not (Const 2)),
+
+            -- combinations
             testEq "a + b + c == 6 with a=1,b=2,c=3" 6 $ eval a1b2c3 (Sum (Var 'a') (Sum (Var 'b') (Var 'c'))),
-            testEq "a + 10 + c == 6 with a=1,b=2,c=3" 6 $ eval a1b2c3 (Sum (Var 'a') (Sum (Const 10) (Var 'c')))
+            testEq "a + 10 + c == 6 with a=1,b=2,c=3" 14 $ eval a1b2c3 (Sum (Var 'a') (Sum (Const 10) (Var 'c'))),
+            testEq "(a < (a + 10)) and (not b) == 6 with a=1,b=2,c=3" 0 $ eval a1b2c3 (And (Less (Var 'a') (Sum (Var 'a') (Const 10))) (Not (Var 'b')))
         ]
 
     astTestCases = evalTests
