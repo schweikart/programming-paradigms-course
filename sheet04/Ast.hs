0e83b6789e933a8cfd4aef4bdcc9398dfd83ba49
Max Schweikart <hello@maxschweik.art>
2023-03-15 18:11:18 +0100

    feat(sheet04): implement expression evaluation (+ tests)

--- sheet04/Ast.hs
+++ sheet04/Ast.hs
@@ -1,5 +1,15 @@
-module Ast (Exp (..)) where
-    data Exp t =            -- t is value type of expression
-        Var t |             -- var specified through t, as requested by exercise 
-        Const t |           -- constant value
-        Sum (Exp t) (Exp t) -- sum of two expressions
+module Ast (Exp (..), Env, eval) where
+    -- simple integer adding expression
+    data Exp t =            -- t is the type containing all variables
+        Var t |             -- variable from the given type
+        Const Integer |     -- Integer = constant value
+        Sum (Exp t) (Exp t) -- with summand expressions
+
+    -- variable environment assigning a value to each variable
+    type Env t = t -> Integer
+
+    -- evaluates a given expression with a given variable assignment
+    eval :: Env t -> Exp t -> Integer
+    eval env (Var t) = env t
+    eval env (Const t) = t
+    eval env (Sum t1 t2) = eval env t1 + eval env t2
--- sheet04/tests/TestAst.hs
+++ sheet04/tests/TestAst.hs
@@ -1,5 +1,18 @@
 module TestAst where
-    import Ast ()
+    import Ast (Env, eval, Exp (Sum, Var, Const))
+    import TestUtils (testEq)
+    
+    a1b2c3 :: Env Char
+    a1b2c3 'a' = 1
+    a1b2c3 'b' = 2
+    a1b2c3 'c' = 3
 
-    astTestCases = []
+    evalTests = [
+            testEq "a == 1 with a=1,b=2,c=3" 1 $ eval a1b2c3 (Var 'a'),
+            testEq "7 == 7 with a=1,b=2,c=3" 7 $ eval a1b2c3 (Const 7),
+            testEq "a + 7 == 8 with a=1,b=2,c=3" 8 $ eval a1b2c3 (Sum (Var 'a') (Const 7)),
+            testEq "a + b + c == 6 with a=1,b=2,c=3" 6 $ eval a1b2c3 (Sum (Var 'a') (Sum (Var 'b') (Var 'c'))),
+            testEq "a + 10 + c == 6 with a=1,b=2,c=3" 6 $ eval a1b2c3 (Sum (Var 'a') (Sum (Const 10) (Var 'c')))
+        ]
 
+    astTestCases = evalTests
