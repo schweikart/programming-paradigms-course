1bd0ddd61a61f5f4e066e3f2c94dee92fb1679f7
Max Schweikart <hello@maxschweik.art>
2023-03-15 20:35:15 +0100

    feat(sheet04): implement rope toList function (+ tests)

--- sheet04/Ropes.hs
+++ sheet04/Ropes.hs
@@ -1,4 +1,4 @@
-module Ropes where
+module Ropes (Rope (..), ropeLength, ropeConcat, toList) where
     data Rope a
         = Leaf [a] -- a fraction of the actual elements of the rope
         | Inner (Rope a) Int (Rope a) -- inner node, containing two children and a *weight*, the length
@@ -11,3 +11,8 @@ module Ropes where
     -- concatenates two ropes
     ropeConcat :: Rope a -> Rope a -> Rope a
     ropeConcat ropeA = Inner ropeA (ropeLength ropeA)
+
+    -- combines the elements of the rope to a signle list
+    toList :: Rope a -> [a]
+    toList (Leaf list) = list
+    toList (Inner left _ right) = toList left ++ toList right
--- sheet04/tests/TestRopes.hs
+++ sheet04/tests/TestRopes.hs
@@ -1,15 +1,18 @@
 module TestRopes where
     import TestUtils (testEq)
-    import Ropes (Rope(Inner, Leaf), ropeLength, ropeConcat)
+    import Ropes (Rope(Inner, Leaf), ropeLength, ropeConcat, toList)
     
     testRope = Inner (Leaf "Hello") 5
-        (Inner (Leaf ", ") 3 (Leaf "orld!"))
+        (Inner (Leaf ", w") 3 (Leaf "orld!"))
 
     ropeLengthTests = [
             testEq "ropeLength Leaf \"test\" == 4" 4 $ ropeLength (Leaf "test"),
             testEq "ropeLength testRope == 13" 13 $ ropeLength testRope
         ]
 
-    -- TODO: test ropeConcat
+    toListTests = [
+            testEq "toList (Leaf \"test\") == \"test\"" "test" $ toList (Leaf "test"),
+            testEq "toList restRope == \"Hello, world\"" "Hello, world!" $ toList testRope
+        ]
 
-    ropesTestCases = ropeLengthTests
+    ropesTestCases = ropeLengthTests ++ toListTests
