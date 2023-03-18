6d6b1bae3f1b7df996698f8ecd21e4c258e86e46
Max Schweikart <hello@maxschweik.art>
2023-03-18 23:40:05 +0100

    feat(tutorial07): implement put for coin list (+ tests)

--- tutorial07/change.pl
+++ tutorial07/change.pl
@@ -1,4 +1,4 @@
-:- module(change, [remove/3]).
+:- module(change, [remove/3, put/3]).
 
 % In the following, "Coins" shall be a list of tuples (CoinValue, CoinAmount)
 
@@ -8,3 +8,12 @@ remove([(C, CoinAmount)|OtherCoins], C, [(C, DecreasedCoinAmount)|OtherCoins]) :
     CoinAmount > 0, DecreasedCoinAmount is CoinAmount - 1.
 remove([(NotC, CoinAmount)|OtherCoins], C, [(NotC, CoinAmount)|RemovedOtherCoins]) :-
     remove(OtherCoins, C, RemovedOtherCoins).
+
+% signature: put(Coins, C, Cnew) where C shall be the value of the coin to
+% insert into Coins, creating Cnew.
+put([(C, CoinAmount)|OtherCoins], C, [(C, IncreasedCoinAmount)|OtherCoins]) :-
+    IncreasedCoinAmount is CoinAmount + 1, !.
+% without this cut, this would yield a choice point that allows inserting at the end too!
+put([(NotC, CoinAmount)|OtherCoins], C, [(NotC, CoinAmount)|NewOtherCoins]) :-
+    put(OtherCoins, C, NewOtherCoins).
+put([], C, [(C, 1)]).
