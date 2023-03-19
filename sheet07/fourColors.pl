9f3ab8c1db06b979b90a8ebeca7de8eb8b8c70fa
Max Schweikart <hello@maxschweik.art>
2023-03-19 11:30:42 +0100

    feat(sheet07): implement three-coloring of the german phone prefix map

--- sheet07/fourColors.pl
+++ sheet07/fourColors.pl
@@ -1,60 +1,112 @@
-:- module(fourColors, [color/1, neighbor/2, germany/8]).
+:- module(fourColors, [ color3/1, neighbor3/2, germany3/8,
+                        color4/1, neighbor4/2, germany4/8]).
 
 % define exactly four colors
-color(blue).
-color(green).
-color(yellow).
-color(red).
+color4(blue).
+color4(green).
+color4(yellow).
+color4(red).
 
 % define which colors are different to each other
-neighbor(blue, green).
-neighbor(blue, yellow).
-neighbor(blue, red).
+neighbor4(blue, green).
+neighbor4(blue, yellow).
+neighbor4(blue, red).
 
-neighbor(green, blue).
-neighbor(green, yellow).
-neighbor(green, red).
+neighbor4(green, blue).
+neighbor4(green, yellow).
+neighbor4(green, red).
 
-neighbor(yellow, blue).
-neighbor(yellow, green).
-neighbor(yellow, red).
+neighbor4(yellow, blue).
+neighbor4(yellow, green).
+neighbor4(yellow, red).
 
-neighbor(red, blue).
-neighbor(red, green).
-neighbor(red, yellow).
+neighbor4(red, blue).
+neighbor4(red, green).
+neighbor4(red, yellow).
 
 % define the topology of the german map, according to phone prefixes
-germany(R02, R03, R04, R05, R06, R07, R08, R09) :-
-    neighbor(R02, R05),
-    neighbor(R02, R06),
+germany4(R02, R03, R04, R05, R06, R07, R08, R09) :-
+    neighbor4(R02, R05),
+    neighbor4(R02, R06),
     
-    neighbor(R03, R04),
-    neighbor(R03, R05),
-    neighbor(R03, R06),
-    neighbor(R03, R09),
+    neighbor4(R03, R04),
+    neighbor4(R03, R05),
+    neighbor4(R03, R06),
+    neighbor4(R03, R09),
 
-    neighbor(R04, R03),
-    neighbor(R04, R05),
+    neighbor4(R04, R03),
+    neighbor4(R04, R05),
 
-    neighbor(R05, R02),
-    neighbor(R05, R03),
-    neighbor(R05, R04),
-    neighbor(R05, R06),
+    neighbor4(R05, R02),
+    neighbor4(R05, R03),
+    neighbor4(R05, R04),
+    neighbor4(R05, R06),
 
-    neighbor(R06, R02),
-    neighbor(R06, R03),
-    neighbor(R06, R05),
-    neighbor(R06, R07),
-    neighbor(R06, R09),
+    neighbor4(R06, R02),
+    neighbor4(R06, R03),
+    neighbor4(R06, R05),
+    neighbor4(R06, R07),
+    neighbor4(R06, R09),
 
-    neighbor(R07, R06),
-    neighbor(R07, R08),
-    neighbor(R07, R09),
+    neighbor4(R07, R06),
+    neighbor4(R07, R08),
+    neighbor4(R07, R09),
 
-    neighbor(R08, R07),
-    neighbor(R08, R09),
+    neighbor4(R08, R07),
+    neighbor4(R08, R09),
     
-    neighbor(R09, R03),
-    neighbor(R09, R06),
-    neighbor(R09, R07),
-    neighbor(R09, R08).
+    neighbor4(R09, R03),
+    neighbor4(R09, R06),
+    neighbor4(R09, R07),
+    neighbor4(R09, R08).
+
+% define exactly three colors
+color3(blue).
+color3(green).
+color3(yellow).
+
+% define which colors are different to each other
+neighbor3(blue, green).
+neighbor3(blue, yellow).
+
+neighbor3(green, blue).
+neighbor3(green, yellow).
+
+neighbor3(yellow, blue).
+neighbor3(yellow, green).
+
+% define the topology of the german map, according to phone prefixes
+germany3(R02, R03, R04, R05, R06, R07, R08, R09) :-
+    neighbor3(R02, R05),
+    neighbor3(R02, R06),
+    
+    neighbor3(R03, R04),
+    neighbor3(R03, R05),
+    neighbor3(R03, R06),
+    neighbor3(R03, R09),
+
+    neighbor3(R04, R03),
+    neighbor3(R04, R05),
+
+    neighbor3(R05, R02),
+    neighbor3(R05, R03),
+    neighbor3(R05, R04),
+    neighbor3(R05, R06),
+
+    neighbor3(R06, R02),
+    neighbor3(R06, R03),
+    neighbor3(R06, R05),
+    neighbor3(R06, R07),
+    neighbor3(R06, R09),
+
+    neighbor3(R07, R06),
+    neighbor3(R07, R08),
+    neighbor3(R07, R09),
+
+    neighbor3(R08, R07),
+    neighbor3(R08, R09),
+    
+    neighbor3(R09, R03),
+    neighbor3(R09, R06),
+    neighbor3(R09, R07),
+    neighbor3(R09, R08).
--- sheet07/fourColors.plt
+++ sheet07/fourColors.plt
@@ -1,21 +1,39 @@
 :- begin_tests(fourColors).
-:- use_module(fourColors, [color/1, neighbor/2, germany/8]).
+:- use_module(fourColors, [ color3/1, neighbor3/2, germany3/8,
+                            color4/1, neighbor4/2, germany4/8]).
 
 % there must be exactly four colors
-test(color) :- findall(C, color(C), Cs), length(Cs, 4).
+test(color4) :- findall(C, color4(C), Cs), length(Cs, 4).
 
 % there must be exactly 3 * 4 = 12 neighbor relationships
-test(neighbor) :- findall((A,B), neighbor(A, B), Ns), length(Ns, 12).
+test(neighbor4) :- findall((A,B), neighbor4(A, B), Ns), length(Ns, 12).
 
 % no neighbor relationship should contain the same color twice
-test(neighbor) :- findall(A, neighbor(A, A), Ns), length(Ns, 0).
+test(neighbor4) :- findall(A, neighbor4(A, A), Ns), length(Ns, 0).
 
 % TODO: test if all neighbor relationships use color atoms only
 
 % there should be exactly 768 ways to color the german phone prefix map using
 % four colors
-test(germany) :-
-    findall((R02,R03,R04,R05,R06,R07,R08,R09), germany(R02,R03,R04,R05,R06,R07,R08,R09), ColorMaps),
+test(germany4) :-
+    findall((R02,R03,R04,R05,R06,R07,R08,R09), germany4(R02,R03,R04,R05,R06,R07,R08,R09), ColorMaps),
     length(ColorMaps, 768).
+
+% there must be exactly three colors
+test(color3) :- findall(C, color3(C), Cs), length(Cs, 3).
+
+% there must be exactly 2 * 3 = 6 neighbor relationships
+test(neighbor3) :- findall((A,B), neighbor3(A, B), Ns), length(Ns, 6).
+
+% no neighbor relationship should contain the same color twice
+test(neighbor3) :- findall(A, neighbor3(A, A), Ns), length(Ns, 0).
+
+% TODO: test if all neighbor relationships use color atoms only
+
+% there should be exactly 6 ways to color the german phone prefix map using
+% three colors
+test(germany3) :-
+    findall((R02,R03,R04,R05,R06,R07,R08,R09), germany3(R02,R03,R04,R05,R06,R07,R08,R09), ColorMaps),
+    length(ColorMaps, 6).
 
 :- end_tests(fourColors).
