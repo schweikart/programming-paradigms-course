7ad14456248e2538d8768cbd87ba29a448b71c72
Max Schweikart <hello@maxschweik.art>
2023-03-19 14:54:33 +0100

    feat(sheet07): implement boat ride

--- sheet07/wolfGoatCabbage.pl
+++ sheet07/wolfGoatCabbage.pl
@@ -4,11 +4,27 @@
 location(left).
 location(right).
 
+other(left, right).
+other(right, left).
+
+check(_, Danger1, Danger2) :- other(Danger1, Danger2).
+check(SameLoc, SameLoc, SameLoc).
+
 % signature: allowed((Man, Goat, Wolf, Cabbage))
 allowed((Man, Goat, Wolf, Cabbage)) :-
     location(Man),
     location(Goat),
     location(Wolf),
     location(Cabbage),
-    ((Wolf \= Goat, !) ; (Man = Wolf, !)),
-    ((Goat \= Cabbage, !) ; (Man = Goat, !)).
+    check(Man, Goat, Wolf),
+    check(Man, Goat, Cabbage).
+
+% signature: ride(S1, F, S2) :- 
+ride((M, G, W, C), empty, (NewM, G, W, C)) :-
+    other(M, NewM).
+ride((MG, MG, W, C), goat, (NewMG, NewMG, W, C)) :-
+    other(MG, NewMG).
+ride((MW, G, MW, C), wolf, (NewMW, G, NewMW, C)) :-
+    other(MW, NewMW).
+ride((MC, G, W, MC), cabbage, (NewMC, G, W, NewMC)) :-
+    other(MC, NewMC).
