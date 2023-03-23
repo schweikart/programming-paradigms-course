37a16b5ca62a78aa9f54c39d88eff676041c79e6
Max Schweikart <hello@maxschweik.art>
2023-03-23 18:14:31 +0100

    feat(tutorial10): implement increase-and-sum

---
+++ tutorial10/increase_and_sum.c
@@ -0,0 +1,48 @@
+#include <mpi/mpi.h>
+#include <stdio.h>
+
+
+int main(int argc, char** args) {
+    // inititalize MPI
+    int rank;
+    int N;
+
+    MPI_Init(&argc, &args);
+    MPI_Comm_size(MPI_COMM_WORLD, &N);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    printf("This is node %i of %i.\n", rank, N);
+
+    // generate numbers
+    int numbers[N];
+    if (rank == 0) {
+        for (int i = 0; i < N; i++) {
+            numbers[i] = i;
+        }
+    }
+
+    // distribute numbers
+    int number;
+    MPI_Scatter(numbers, 1, MPI_INT, &number, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // increase number on each node
+    number++;
+
+    // collect numbers
+    MPI_Gather(&number, 1, MPI_INT, &numbers, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // aggregate collected numbers
+    if (rank == 0) {
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += numbers[i];
+        }
+
+        printf("Sum: %i\n", sum);
+    }
+
+    // clean up MPI
+    MPI_Finalize();
+    
+    return 0;
+}
